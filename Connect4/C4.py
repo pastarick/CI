069a57d06b9c940c6aa2379fc6d4ddbfbd2c7444
Riccardo Sepe <seric814@gmail.com>
2021-11-17 20:52:58 +0100

    Implement4ed utility2 function (works better)

--- Connect4/C4.py
+++ Connect4/C4.py
@@ -15,6 +15,15 @@ MAGIC_SQUARE = np.array([[16, 2, 3, 13],
 AI_PLAYER = -1
 USER_PLAYER = 1
 
+EVALUATION_TABLE = np.rot90(np.array([[3, 4, 5, 7, 5, 4, 3],
+                                [4, 6, 8, 10, 8, 6, 4],
+                                [5, 8, 11, 13, 11, 8, 5],
+                                [5, 8, 11, 13, 11, 8, 5],
+                                [4, 6, 8, 10, 8, 6, 4],
+                                [3, 4, 5, 7, 5, 4, 3]]), 3)
+
+# AI_TABLE = USER_TABLE.copy()
+
 
 def valid_moves(board):
     """Returns columns where a disc may be played"""
@@ -36,32 +45,32 @@ def take_back(board, column):
 def four_in_a_row(board, player):
     """Checks if `player` has a 4-piece line"""
     return (
-        any(
-            np.all(board[c, r] == player)
-            for c in range(NUM_COLUMNS)
-            for r in (list(range(n, n + FOUR)) for n in range(COLUMN_HEIGHT - FOUR + 1))
-        )
-        or any(
-            np.all(board[c, r] == player)
-            for r in range(COLUMN_HEIGHT)
-            for c in (list(range(n, n + FOUR)) for n in range(NUM_COLUMNS - FOUR + 1))
-        )
-        or any(
-            np.all(board[diag] == player)
-            for diag in (
-                (range(ro, ro + FOUR), range(co, co + FOUR))
-                for ro in range(0, NUM_COLUMNS - FOUR + 1)
-                for co in range(0, COLUMN_HEIGHT - FOUR + 1)
+            any(
+                np.all(board[c, r] == player)
+                for c in range(NUM_COLUMNS)
+                for r in (list(range(n, n + FOUR)) for n in range(COLUMN_HEIGHT - FOUR + 1))
             )
+            or any(
+        np.all(board[c, r] == player)
+        for r in range(COLUMN_HEIGHT)
+        for c in (list(range(n, n + FOUR)) for n in range(NUM_COLUMNS - FOUR + 1))
+    )
+            or any(
+        np.all(board[diag] == player)
+        for diag in (
+            (range(ro, ro + FOUR), range(co, co + FOUR))
+            for ro in range(0, NUM_COLUMNS - FOUR + 1)
+            for co in range(0, COLUMN_HEIGHT - FOUR + 1)
         )
-        or any(
-            np.all(board[diag] == player)
-            for diag in (
-                (range(ro, ro + FOUR), range(co + FOUR - 1, co - 1, -1))
-                for ro in range(0, NUM_COLUMNS - FOUR + 1)
-                for co in range(0, COLUMN_HEIGHT - FOUR + 1)
-            )
+    )
+            or any(
+        np.all(board[diag] == player)
+        for diag in (
+            (range(ro, ro + FOUR), range(co + FOUR - 1, co - 1, -1))
+            for ro in range(0, NUM_COLUMNS - FOUR + 1)
+            for co in range(0, COLUMN_HEIGHT - FOUR + 1)
         )
+    )
     )
 
 
@@ -82,11 +91,11 @@ def print_board(board):
 
     print("-", end='')
     for i in range(NUM_COLUMNS):
-        print("-"*4, end='')
+        print("-" * 4, end='')
     print('')
 
     for i in range(NUM_COLUMNS):
-        print(f"| {i+1} ", end='')
+        print(f"| {i + 1} ", end='')
     print('|')
 
 
@@ -135,20 +144,20 @@ def is_terminal(board):
 def utility(board, cell):
     player = board[cell[0], cell[1]]
     # tmp_board is the board with only the moves played by ‘player‘
-    tmp_board = (board.copy() + player*np.absolute(board))//2
+    tmp_board = (board.copy() + player * np.absolute(board)) // 2
 
     # i, j are the coordinates of the top-leftmost cell in sub square
     # OBS: j won't change over the loop
-    i = max(0, cell[0]-FOUR+1)
-    j = max(0, cell[1]-FOUR+1)
+    i = max(0, cell[0] - FOUR + 1)
+    j = max(0, cell[1] - FOUR + 1)
 
     # x, y are the coordinates of current cell in sub square
     # OBS: also y won't change over the loop
     y = cell[1] - j
 
     m = 0
-    while i <= NUM_COLUMNS-FOUR:
-        sub = tmp_board[i:i+4, j:j+4]
+    while i <= NUM_COLUMNS - FOUR:
+        sub = tmp_board[i:i + 4, j:j + 4]
 
         # tmp_sub is the result of applying the MAGIC_SQUARE mask to sub
         tmp_sub = sub * MAGIC_SQUARE
@@ -169,7 +178,7 @@ def utility(board, cell):
                 m = min(m, np.trace(tmp_sub))
 
         # if cell is on the antidiagonal (even size -> cell cannot be on both diag and antidiag)
-        if x+y == FOUR-1:
+        if x + y == FOUR - 1:
             if player == USER_PLAYER:
                 m = max(m, np.trace(np.flip(tmp_sub)))
             else:
@@ -179,12 +188,38 @@ def utility(board, cell):
     return m
 
 
+def num_covered(board, cell, player):
+    c = 0
+    x = cell[0]
+    y = cell[1]
+
+    # horizontal
+    i1 = max(0, y-FOUR+1)
+    i2 = min(NUM_COLUMNS, y+FOUR)
+    c += np.count_nonzero(board[x, i1:i2] == -player)
+
+    # vertical
+    j1 = max(0, x-FOUR+1)
+    j2 = min(COLUMN_HEIGHT, x+FOUR)
+    c += np.count_nonzero(board[i1:i2, y] == -player)
+
+    # diag1
+
+
+def utility2(board):
+    # player = board[cell[0], cell[1]]
+    # vedo se nelle celle coperte da cell c'è -player
+
+    return int(np.sum(board * EVALUATION_TABLE))
+
+
 def minmax(board, move, depth, player, alpha, beta):
-    if move is not None and (is_terminal(board) or depth) == 0:
+    if move is not None and (is_terminal(board) or depth == 0):
         (index,) = [i for i, v in np.ndenumerate(board[move]) if v != 0][-1]
         cell = (move, index)
-        return move, int(player * utility(board, cell))
+        return move, utility2(board)  # int(player * utility(board, cell))
 
+    # print(f"depth = {depth}")
     if player == AI_PLAYER:
         v1 = (None, np.inf)
         for m in valid_moves(board):
@@ -203,7 +238,7 @@ def minmax(board, move, depth, player, alpha, beta):
         for m in valid_moves(board):
             # alpha = max(alpha, minmax(board, m, depth-1, -player))
             play(board, m, player)
-            v2 = minmax(board, m, depth-1, -player, alpha, beta)
+            v2 = minmax(board, m, depth - 1, -player, alpha, beta)
             take_back(board, m)
             if v2[1] > v1[1]:
                 v1 = v2
@@ -247,16 +282,17 @@ def main():
     player = -player
     while True:
         if player == AI_PLAYER:
+
             ply = ai_move(board)
-            print(f"AI playing column {ply+1}...")
+            print(f"AI playing column {ply + 1}...")
             play(board, ply, player)
             print_board(board)
             if four_in_a_row(board, player):
                 print(f"AI won the game.")
                 break
         else:
-            ply = int(input("Please chose the column of your move: "))-1
-            print(f"You played column {ply+1}.")
+            ply = int(input("Please chose the column of your move: ")) - 1
+            print(f"You played column {ply + 1}.")
             play(board, ply, player)
             print_board(board)
             if four_in_a_row(board, player):
@@ -265,7 +301,8 @@ def main():
 
         player = -player
 
+    print("Terminating game")
+
 
 if __name__ == '__main__':
     main()
-
