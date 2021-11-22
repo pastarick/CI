bd6466085d7a0a8d7adb4d94f62c9c9d493a1105
Riccardo Sepe <seric814@gmail.com>
2021-11-22 22:28:48 +0100

    main implementation

--- Connect4/main.py
+++ Connect4/main.py
@@ -5,7 +5,8 @@ def main():
     game = Connect4(num_players=1)
 
     print(game)
-    game.play_game_montecarlo()
+    # game.play_game_montecarlo()
+    game.play_game_minmax()
 
 
 if __name__ == '__main__':
